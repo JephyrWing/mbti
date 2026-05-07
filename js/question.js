e81d55bba52ac8a3ea0e2c263469a311828a23b7
JephyrWing <gad03302@gmail.com>
2026-05-08 08:40:24 +0900

    feat: question  까지 완료

---
+++ js/question.js
@@ -0,0 +1,44 @@
+import { questions } from "../data/data.js";
+
+// 사용할 태그를 변수에 지정
+// Progress의 value
+const questionBox = document.querySelector(".box_bg");
+const progressValue = document.querySelector(".progress .value");
+const numberEl = document.querySelector(".number");
+const questionEl = document.querySelector(".question");
+const choice1El = document.querySelector(".choice1");
+const choice2El = document.querySelector(".choice2");
+renderQuestion(0);
+
+let currentNumber = 1;
+let mbti = "";
+
+// 1. EventListener를 두개 붙인다.
+choice1El.addEventListener("click", function(){nextQuestion(0)});
+choice2El.addEventListener("click", function(){nextQuestion(1)});
+
+// 2. 질문번호를 클릭하면
+function nextQuestion(choiceNumber) {
+  // - currentNumber를 하나 증가시킨다.
+  // - 결과를 mbti 변수에 저장한다.
+  if (currentNumber >= 10){
+      mbti = mbti + questions[currentNumber - 1].choices[choiceNumber].value;
+    console.log(mbti);
+    console.log('result.html로 이동');
+  } else {
+    renderQuestion(currentNumber);
+      mbti = mbti + questions[currentNumber - 1].choices[choiceNumber].value;
+    currentNumber ++;
+  }
+}  
+  
+// 3. 마지막 질문인지 확인해서 결과페이지로 넘긴다.
+// result.html -> console.log로 출력만
+function renderQuestion(currentNumber) {
+  questionBox.style = `background-image : url("./images/box${currentNumber}.jpg")`
+  numberEl.innerHTML = questions[currentNumber].number;
+  questionEl.innerHTML = questions[currentNumber].question;
+  choice1El.innerHTML = questions[currentNumber].choices[0].text;
+  choice2El.innerHTML = questions[currentNumber].choices[1].text;
+  progressValue.style.width = (currentNumber + 1) * 10 + "%";
+}
